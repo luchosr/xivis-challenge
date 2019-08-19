ae596886e40a32d72c95d457c7be2677cb5bd391
Luciano <luchosr@gmail.com>
2019-08-18 23:28:27 -0300

    fix cart display

--- src/Cart.js
+++ src/Cart.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 
 const Cart = () => {
   const [cart, setCart] = useState(JSON.parse(localStorage.getItem("carrito")));
+  const [carrito, setCarrito] = useState(!(cart >= 0));
+  // ---> https://blog.campvanilla.com/javascript-the-curious-case-of-null-0-7b131644e274
+
   return (
     <div>
       <div className="cart__bar">
         <h2>Items en tu carrito</h2>
       </div>
+
       <ul className="cart__list">
-        {cart.length > 0 &&
+        {cart >= 0 && <li>No hay items en tu carrito</li>}
+        {carrito &&
           cart.map(item => (
             <li className="cart__list__item">
               <div className="cart__item__description">
